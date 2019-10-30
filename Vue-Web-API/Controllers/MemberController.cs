75f42ad6a37516215459b45b7d30a390d663665b
Shady Yehia <shady.yahia@itworx.com>
2019-10-30 05:56:36 +0200

    initial commit

---
+++ Vue-Web-API/Controllers/MemberController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocietyManagement.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+
+        public MemberController()
+        {
+
+        }
+        // GET: api/Members
+        [HttpGet]
+        [ActionName("getMembersList")]
+        public IEnumerable<MembersViewModel> GetMembers()
+        {
+            var listMembers = new List<MembersViewModel>() {
+                new MembersViewModel() { firstName="shady",lastName="yehia",address="NasrCity",occupier=1,mntncPaidFreq=5,maintenance=4,memberId="355xc4r4"},
+            new MembersViewModel() { firstName = "shady", lastName = "yehia", address = "NasrCity", occupier = 1, mntncPaidFreq = 5, maintenance = 4, memberId = "355xc4r4" },
+            new MembersViewModel() { firstName = "shady", lastName = "yehia", address = "NasrCity", occupier = 1, mntncPaidFreq = 5, maintenance = 4, memberId = "355xc4r4" }
+
+            };
+            return listMembers;
+        }
+
+    
+
+        public class MembersViewModel
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string address { get; set; }
+            public int gender { get; set; }
+            public int occupier { get; set; }
+            public int mntncPaidFreq { get; set; }
+            public int maintenance { get; set; }
+            public string memberId { get; set; }
+          
+        }
+    }
+}
---
+++ Vue-Web-API/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Vue_Web_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           var clientURL  = Configuration.GetSection("AppSettings:ClientURL");
+            var corsPolicyName= Configuration.GetSection("AppSettings:CorspolicyName");
+            services.AddCors(options =>
+            {
+                options.AddPolicy(corsPolicyName.Value, builder =>
+                {
+                    builder
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials()
+                      .WithOrigins(clientURL.Value);
+                });
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var corsPolicyName = Configuration.GetSection("AppSettings:CorspolicyName");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseCors(corsPolicyName.Value);
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
